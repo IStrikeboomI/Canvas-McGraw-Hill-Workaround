0949c56228abf95a7a142a35db04797afc9e5347
Shubh Makadia <60586926+IStrikeboomI@users.noreply.github.com>
2023-01-26 22:17:42 -0600

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const button = document.getElementById("ContentPlaceHolder1_UserClasses1_RepeaterCourses_UserClassesCommon_0_AddedServices2_0_connectMathlink_0");
+if (button) {
+	button.target = "_self";
+}
